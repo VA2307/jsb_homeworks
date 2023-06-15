34c9f618fd85a2ac3d814fa13ef95dca2a7673ee
Vladimir Alekseev <vega23071235789@gmail.com>
2023-06-15 05:36:26 +0100

    fix: some fixes in 1.1 and 1.2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,51 +3,17 @@ function solveEquation(a, b, c) {
   let d = (b ** 2 -(4 * a * c));
   let arr = [];
 
-  // if (d === 0) {
-  //   arr.push(-b / (2 * a));
-  // } else if (d > 0) {
-  //   arr.push((-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a));
-  // }
-
-  switch (true) {
-    case (d === 0) :
-      arr.push(-b / (2 * a));
-      break;
-    case (d > 0) :
-      arr.push((-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a));
-      break;
+  if (d === 0) {
+    arr.push(-b / (2 * a));
+  } else if (d > 0) {
+    arr.push((-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a));
   }
-  
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  debugger;
-  if (typeof percent === 'string' || typeof percent === 'number') {
-    percent === +percent;
-  } else {
-    percent === false;
-  }
-
-  if (typeof contribution === 'string' || typeof contribution === 'number') {
-    contribution === +contribution;
-  } else {
-    contribution === false;
-  }
-
-  if (typeof amount === 'string' || typeof amount === 'number') {
-    amount === +amount;
-  } else {
-    amount === false;
-  }
-
-  if (typeof countMonths === 'string' || typeof countMonths === 'number') {
-    countMonths === +countMonths;
-  } else {
-    countMonths === false;
-  }
-
-  let p = percent / 100;
+  let p = percent / 100 / 12;
   let s = amount - contribution;
   let payPerMonth = s * (p + (p / (((1 + p) ** countMonths) - 1)))
 
